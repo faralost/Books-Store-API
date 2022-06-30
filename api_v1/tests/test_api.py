8e07ef8c03dc9b6c125eab0e2b86d2bfc5262051
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-30 16:36:48 +0600

    add test for filtered list of books

--- api_v1/tests/test_api.py
+++ api_v1/tests/test_api.py
@@ -7,11 +7,20 @@ from store.models import Book
 
 
 class BookApiTestCase(APITestCase):
+    def setUp(self):
+        self.book1 = Book.objects.create(name='Test Book 1', price=100, author_name='Author 1')
+        self.book2 = Book.objects.create(name='Test Book 2', price=200, author_name='Author 2')
+        self.book3 = Book.objects.create(name='Test Book 3', price=100, author_name='Author 3')
+        self.url = reverse('api_v1:book-list')
+
     def test_get_list_of_books(self):
-        book1 = Book.objects.create(name='Test Book 1', price=100)
-        book2 = Book.objects.create(name='Test Book 2', price=200)
-        url = reverse('api_v1:book-list')
-        response = self.client.get(url)
-        serializer_data = BookSerializer([book1, book2], many=True).data
+        response = self.client.get(self.url)
+        serializer_data = BookSerializer([self.book1, self.book2, self.book3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_list_of_filtered_books(self):
+        response = self.client.get(self.url, data={'price': 100})
+        serializer_data = BookSerializer([self.book1, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
