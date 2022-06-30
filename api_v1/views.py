da5aea18e2af234fd7f614518c77fc73fc8ed839
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-30 16:16:55 +0600

    add search by name and author_name

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from api_v1.serializers import BookSerializer
 from store.models import Book
@@ -8,5 +9,6 @@ from store.models import Book
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
