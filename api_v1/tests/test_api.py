dfe97358bf50eb07c517e64a64269283d61684bc
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-30 23:26:52 +0600

    add test for updating book

--- api_v1/tests/test_api.py
+++ api_v1/tests/test_api.py
@@ -17,34 +17,35 @@ class BookApiTestCase(APITestCase):
         self.book1 = Book.objects.create(name='Test Book 1', price=100, author_name='Author 1')
         self.book2 = Book.objects.create(name='Test Book 2', price=200, author_name='Author 2')
         self.book3 = Book.objects.create(name='Test Book 3 Author 1', price=100, author_name='Author 3')
-        self.url = reverse('api_v1:book-list')
+        self.url_list = reverse('api_v1:book-list')
+        self.url_detail = reverse('api_v1:book-detail', args=(self.book1.id,))
 
     def test_get_list_of_books(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url_list)
         serializer_data = BookSerializer([self.book1, self.book2, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
     def test_get_list_of_filtered_books_by_price(self):
-        response = self.client.get(self.url, data={'price': 100})
+        response = self.client.get(self.url_list, data={'price': 100})
         serializer_data = BookSerializer([self.book1, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
     def test_get_list_of_searched_books_by_name_and_author_name(self):
-        response = self.client.get(self.url, data={'search': 'Author 1'})
+        response = self.client.get(self.url_list, data={'search': 'Author 1'})
         serializer_data = BookSerializer([self.book1, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
     def test_get_list_of_ordered_books_by_author_name_descending(self):
-        response = self.client.get(self.url, data={'ordering': '-author_name'})
+        response = self.client.get(self.url_list, data={'ordering': '-author_name'})
         serializer_data = BookSerializer([self.book3, self.book2, self.book1], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
     def test_get_list_of_ordered_books_by_price_ascending(self):
-        response = self.client.get(self.url, data={'ordering': 'price'})
+        response = self.client.get(self.url_list, data={'ordering': 'price'})
         serializer_data = BookSerializer([self.book1, self.book3, self.book2], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
@@ -58,9 +59,22 @@ class BookApiTestCase(APITestCase):
         }
         json_data = json.dumps(data)
         self.client.force_login(self.user)
-        response = self.client.post(self.url, data=json_data, content_type='application/json')
+        response = self.client.post(self.url_list, data=json_data, content_type='application/json')
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(4, Book.objects.all().count())
         new_book = Book.objects.get(id=4)
         self.assertEqual('New Book of Pyhon 3', new_book.name)
         self.assertEqual(666, new_book.price)
+
+    def test_update_book(self):
+        data = {
+            'name': self.book1.name,
+            'price': 666,
+            'author_name': self.book1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(self.url_detail, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book1.refresh_from_db()
+        self.assertEqual(666, self.book1.price)
