22aea06257fff9ea6bda2989bd37573dc834b51f
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-30 16:50:24 +0600

    refactored test_serializers.py by adding author_name

--- api_v1/tests/test_serializers.py
+++ api_v1/tests/test_serializers.py
@@ -6,19 +6,21 @@ from store.models import Book
 
 class BookSerializerTestCase(TestCase):
     def test_serializer_is_ok(self):
-        book1 = Book.objects.create(name='Test Book 1', price=100)
-        book2 = Book.objects.create(name='Test Book 2', price=200)
+        book1 = Book.objects.create(name='Test Book 1', price=100, author_name='Author 1')
+        book2 = Book.objects.create(name='Test Book 2', price=200, author_name='Author 2')
         data = BookSerializer([book1, book2], many=True).data
         expected_data = [
             {
                 'id': book1.id,
                 'name': 'Test Book 1',
-                'price': '100.00'
+                'price': '100.00',
+                'author_name': 'Author 1'
             },
             {
                 'id': book2.id,
                 'name': 'Test Book 2',
-                'price': '200.00'
+                'price': '200.00',
+                'author_name': 'Author 2'
             },
         ]
         self.assertEqual(expected_data, data)
