812c2128b3586ddc644b5ded38fb2605f2c453a3
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-30 15:14:02 +0600

    create unit test for Book serializer

--- api_v1/tests/test_serializers.py
+++ api_v1/tests/test_serializers.py
@@ -1,5 +1,24 @@
 from django.test import TestCase
 
+from api_v1.serializers import BookSerializer
+from store.models import Book
+
 
 class BookSerializerTestCase(TestCase):
-    pass
+    def test_serializer_is_ok(self):
+        book1 = Book.objects.create(name='Test Book 1', price=100)
+        book2 = Book.objects.create(name='Test Book 2', price=200)
+        data = BookSerializer([book1, book2], many=True).data
+        expected_data = [
+            {
+                'id': book1.id,
+                'name': 'Test Book 1',
+                'price': '100.00'
+            },
+            {
+                'id': book2.id,
+                'name': 'Test Book 2',
+                'price': '200.00'
+            },
+        ]
+        self.assertEqual(expected_data, data)
